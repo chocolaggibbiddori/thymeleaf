7f31fa7371fbfd5d4e6bdd57a5bbc9e3e757bb68
chocolaggibbiddori <didrkd684@naver.com>
2022-11-16 20:10:39 +0900

    feat : 기본 객체들, 편의 객체

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,13 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -49,6 +51,13 @@ public class BasicController {
         return "/basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+
+        return "/basic/basic-objects";
+    }
+
     @Data
     static class User {
 
@@ -58,6 +67,13 @@ public class BasicController {
         public User(String username, int age) {
             this.username = username;
             this.age = age;
+        }
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
         }
     }
 }
